2fc69d9b99f5e18855767888ce524567528f10bc
Magnon Souza <mprs@cin.ufpe.br>
2019-11-24 15:59:18 -0300

    crossing added and dna fixes

--- apib.py
+++ apib.py
@@ -1,29 +1,38 @@
 from random import sample, randint
-from dna import DNA
+import spotify, json
 
-def gen_individual(track, features):
+def gen_individual(track, features, seed_genres):
     ind = {}
 
-    ind['id'] = track['id']
-    ind['popularity'] = track['popularity']
+    ind['track_name'] = track['name']
+    ind['track_id'] = track['id']
+    ind['artist_name'] = track['artists'][0]['name']
+    ind['artist_id'] = track['artists'][0]['id']
+    ind['preview_url'] = track['preview_url']
+
+    ind['s_artists'] = None
+    ind['s_tracks'] = None
+    ind['s_genres'] = seed_genres
+
+    ind['acousticness'] = features['acousticness']
     ind['danceability'] = features['danceability']
     ind['energy'] = features['energy']
-    ind['loudness'] = features['loudness']
-    ind['mode'] = features['mode']
-    ind['speechiness'] = features['speechiness']
-    ind['acousticness'] = features['acousticness']
     ind['instrumentalness'] = features['instrumentalness']
     ind['liveness'] = features['liveness']
-    ind['valence'] = features['valence']
+    ind['loudness'] = features['loudness']
+    ind['mode'] = features['mode']
+    ind['popularity'] = track['popularity']
+    ind['speechiness'] = features['speechiness']
     ind['tempo'] = features['tempo']
+    ind['valence'] = features['valence']
 
     return ind
 
-def init_population(n_parents, initial_playlist, features_dict):
+def init_population(initial_playlist, features_dict, seed_genres):
     population = []
 
-    for i in range(n_parents):
-        indiv = gen_individual(initial_playlist['items'][i]['track'], features_dict[i])
+    for i in range(len(initial_playlist)):
+        indiv = gen_individual(initial_playlist[i]['track'], features_dict[i], seed_genres)
         population.append((-1, indiv)) # -1 is the initial fit
     
     return population
@@ -63,6 +72,34 @@ def parent_select(population, n_parents=2):
     
     return parents
 
-def nsfw(parents):
-    child = DNA()
+def mutation(child):
+    child['popularity'] = (randint(0, 100) + child['popularity']) % 100
 
+def nsfw(parents, sp):
+    p1 = list(parents[0][1].items())[7:13]
+    p2 = list(parents[1][1].items())[13:]
+    seeds = [('s_artists', [parent[1]['artist_id'] for parent in parents]), \
+            ('s_tracks', [parent[1]['track_id'] for parent in parents])]
+
+    child = dict([('track_name', '_'), ('track_id', '_'), ('artist_name', '_'), \
+                    ('artist_id', '_'), ('preview_url', '_')] + seeds + p1 + p2)
+    mutation(child)
+    targets = target_dict(child)
+    track = sp.get_track(child, targets)['tracks'][0]
+    new_feat = sp.get_features(track)
+    update_details(child, track, new_feat)
+    return (-1, child)
+
+def update_details(child, track, features):
+    child['track_name'] = track['name']
+    child['track_id'] = track['id']
+    child['artist_name'] = track['artists'][0]['name']
+    child['artist_id'] = track['artists'][0]['id']
+    child['preview_url'] = track['preview_url']
+    for k in features:
+        if k in child:
+            child[k] = features[k]
+
+def target_dict(features):
+    targets = {f'target_{i}': j for i, j in features.items()}
+    return targets
